b9c2cce1ef38293ef5110929febb3d929641b22b
Duval Cash <duvalwcash@gmail.com>
2019-11-05 15:47:27 -0500

    refactored footer to partial and copyright to helper, positioned contact form accordingly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
       when "alert" then "is-warning"
     end
   end
+
+  def copyright
+    link_to "\u00A9 Copyright Duval the Dev #{Time.now.year}. All rights reserved.", "https://github.com/bvdmind/duval-the-dev"
+  end
 end
