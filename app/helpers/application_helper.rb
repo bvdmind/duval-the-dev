9a89b30fc4cad8514fce9c7d0fc230bff82c80b2
Duval Cash <duvalwcash@gmail.com>
2019-11-05 16:28:03 -0500

    Implemented helper to randomaize positioning of content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,9 @@ module ApplicationHelper
   def copyright
     link_to "\u00A9 Copyright Duval the Dev #{Time.now.year}. All rights reserved.", "https://github.com/bvdmind/duval-the-dev"
   end
+
+  def main_content_position_sampler
+    variations = [render("layouts/main_left"), render("layouts/main_right")]
+    variations.sample
+  end
 end
